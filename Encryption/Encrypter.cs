f178d3595db0ff8e5d7dad0e7449890389e59b4f
Allen <allenho388@gmail.com>
2013-02-27 12:01:21 -0800

    Initial Push (minus tests)
    This is a working simple XOR Encryption program. It is a copy of the old
    version that I had made in C a long time ago. I decided to begin the
    project again in C# and give it a nice GUI as well as multiple file
    support and multiple encryption support.

---
+++ Encryption/Encrypter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//referenced
+using System.IO; //StreamReader , StreamWriter
+
+namespace Encryption
+{
+    class Encrypter
+    {
+        private byte[] EncryptData(byte[] text, String key)
+        {
+            //Creates a character array for the result (should not hit above 4096)
+            byte[] result = new byte[text.Length];
+
+            //Doubt it matters that much, but for the speed!
+            int keyLength = key.Length-1;
+
+            //Perform XOR encryption rotating the key
+            for (int i = 0, keyCounter = 0; i < text.Length; i++, keyCounter++)
+            {
+                //reaching end of the key, reset the index to beginning
+                if (keyCounter > keyLength)
+                {
+                    keyCounter = 0;
+                }
+                //XOR  the data
+                result[i] = (byte) (text[i] ^ key[keyCounter]);
+            }
+            
+            //return the bytes
+            return result;
+        }
+
+        public void EncryptFile(String fileName, String key, String outputName)
+        {
+            /*
+             * Using FileStream now because StreamReader was not outputting the data properly
+             * Files need to be read and written using BinaryReader in order to properly preserve the data as well as ensure proper encryption
+             */
+            FileStream inFile = new FileStream(@fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader inRead = new BinaryReader(inFile);
+
+            //Delete existing files
+            if (File.Exists(@outputName))
+            {
+                File.Delete(@outputName);
+            }
+
+            //Creating the FileWriter and the new File we are writing to
+            FileStream outFile = new FileStream(@outputName, FileMode.CreateNew);
+            BinaryWriter outWrite = new BinaryWriter(outFile);
+
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int success;
+            while ((success = inFile.Read(buffer, 0, bufferSize)) > 0)
+            {
+                //Console.WriteLine(success);
+                outWrite.Write(EncryptData(buffer,key), 0, success);
+            }
+
+            inRead.Close();
+            inFile.Close();
+            outWrite.Close();
+            outFile.Close();
+
+        }
+    }
+}
---
+++ Encryption/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//begin added references
+using System.Windows.Forms; // FolderBrowserDialog, DialogResult
+using System.IO; // Directory
+using System.Collections; //ArrayList
+
+namespace Encryption
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String currentDirectory;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Simple Input File Folder Browsing
+         * Allows user to select a directory from which to choose their file(s).
+         * This is in preparation for the multiple files version..
+         * Although I probably should have allowed for individual file selections first probably...
+         */
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            //Creates a folder browsing object
+            FolderBrowserDialog fb = new System.Windows.Forms.FolderBrowserDialog();
+            DialogResult result;
+            String folderName = "";
+
+            // Reloads the most recently accessed folder. need to write this to the app.config. ON TODO LIST
+            if (currentDirectory != "")
+            {
+                fb.SelectedPath = currentDirectory;
+            }
+
+            /* Loop brings up the folder browsing dialog and forces user to pick a folder.
+             * If none is selected, it will re-prompt until they pick one*/
+            while (folderName.Equals(""))
+            {
+                // ShowDialog brings up brings up the browsing window
+                result = fb.ShowDialog();
+                //get selected path
+                folderName = fb.SelectedPath;
+            }
+
+            //Get the filePaths from the folder
+            String[] filePaths = Directory.GetFiles(@folderName); //note: @ is used in front of folderName so that we do not need to escape the '\' symbols.
+            currentDirectory = fb.SelectedPath;
+
+            //Array to hold our filenames
+            ArrayList fileNames = new ArrayList();
+            //Get just the filenames
+            foreach (String s in filePaths)
+            {
+                String[] split = s.Split('\\');
+                fileNames.Add(split[split.Length - 1]);
+            }
+
+            //Clean out the filenames box.
+            fileNameBox.Items.Clear();
+            //Add the files to the combobox
+            foreach (String s in fileNames)
+            {
+                fileNameBox.Items.Add(s);
+            }
+        }
+        
+        /*
+         * Encryption Button Click.
+         * Takes the user provided input (if not given, will alert user)
+         * Generates an output name if necessary
+         * passes information to Encrypter to perform the Encryption
+         */
+        private void encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            //Generates the input FilePath
+            String inputFileName = currentDirectory + "\\" + fileNameBox.SelectedValue;
+            //Generates the output FilePath
+            String outputFileName = currentDirectory + "\\" +  outputBox.Text;
+            
+            //If no output name is provided, we will generate one for the user (should we prompt?)
+            if (outputBox.Text == "")
+            {
+                outputFileName += generateGenericName(fileNameBox.SelectedValue.ToString());
+            }
+            
+            //Load Encrypter and pass information to begin encryption
+            Encrypter en = new Encrypter();
+            en.EncryptFile(@inputFileName, keyBox.Password.ToString(), @outputFileName);
+        }
+
+        /*
+         * Method to Generate a generic output name in case one is not given
+         */
+        private String generateGenericName(String name)
+        {
+            //splits on the extension ( multiple . case handled )
+            String[] split = name.Split('.');
+
+            StringBuilder fileName = new StringBuilder();
+            //For the multiple . case, will append all but the "extension"
+            for (int i = 0; i < split.Length - 1; i++)
+            {
+                fileName.Append(split[i]);
+                fileName.Append(".");
+            }
+            return fileName + "e";
+        }
+
+        private void testEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
